2972bdb3773b9746bc4fa66ce0726166936d0ac1
Rizxqi <rizqidewanto23@gmail.com>
2025-03-01 05:26:50 +0700

    Pengerjaan POS Jobsheet 2

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,18 +1,25 @@
-<?php
-
+<?php   
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Home Page
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+// Products Page
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+
+// User Page
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Sales Page
+Route::get('/sales', [SalesController::class, 'index']);
